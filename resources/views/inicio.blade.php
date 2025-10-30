b9423b76f1cd95098dcc129bf852701e7c6fdd9f
YeroverAli <yeroveraligonzalez@alumno.ieselrincon.es>
2025-10-30 13:45:57 +0000

    Tailwindcss

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -1,11 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <h1>fhsehffvrdgvgdrgrgh</h1>
-</body>
+<!doctype html>
+<html>
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    @vite('resources/css/app.css')
+  </head>
+  <body>
+    <h1 class="text-amber-300 font-bold underline">
+      Hello world!
+    </h1>
+  </body>
 </html>
