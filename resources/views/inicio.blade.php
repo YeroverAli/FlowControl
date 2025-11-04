be820943add210abe2040bf88b0cc8d9ea19b47a
YeroverAli <yeroveraligonzalez@alumno.ieselrincon.es>
2025-11-04 13:55:08 +0000

    Pagina de bienvenida modificada

--- resources/views/inicio.blade.php
+++ resources/views/inicio.blade.php
@@ -5,9 +5,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     @vite('resources/css/app.css')
   </head>
-  <body>
-    <h1 class="text-amber-300 font-bold underline">
-      Hello world!
-    </h1>
+  <body class="bg-[linear-gradient(90deg,#d1f4b6,#66dfbc,#00c2da,#009df2,#476ade)]
+   font-serif font-bold m-1">
+    <div class= "flex justify-between my-2">
+      <h1 class="text-5xl text-blue-500 font-bold">
+        Flow Control
+      </h1>
+      <button class="rounded-full p-2 bg-black text-white hover:bg-white hover:text-black">
+        Iniciar Sesión
+      </button>
+    </div>
+    <div class="my-10 flex">
+      <h2 class="text-gray-700 max-w-3xl mt-6">
+      <span class="text-5xl font-extrabold text-amber-300">Flow Control</span> es la 
+        plataforma de gestión de proyectos más intuitiva y visual 
+        diseñada para transformar el caos en claridad operativa. Utilizando la 
+        metodología Kanban, nuestra aplicación permite a equipos de todos los tamaños 
+        visualizar instantáneamente el progreso de sus tareas mediante tableros dinámicos 
+        de arrastrar y soltar. Olvídese de las cadenas de correos y los archivos perdidos; 
+        con funcionalidades de asignación directa, checklists detalladas y colaboración en 
+        tiempo real, ofrecemos un ecosistema centralizado donde cada proyecto avanza sin 
+        fricciones, asegurando que su equipo se mantenga enfocado en lo que realmente 
+        importa: entregar resultados.
+      </h2>
+
+    </div>
+
   </body>
 </html>
