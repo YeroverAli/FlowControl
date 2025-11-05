30fbe6c2dbb38ccc971bd4521542961e93894eac
BrianDev06 <brianvegacalvo@alumno.ieselrincon.es>
2025-11-05 12:14:13 +0000

    Diseño creación formulario mejorado + enlace al main del proyecto

--- resources/views/formulario/crear.blade.php
+++ resources/views/formulario/crear.blade.php
@@ -7,29 +7,42 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-[linear-gradient(90deg,#d1f4b6,#66dfbc,#00c2da,#009df2,#476ade)] font-monofont-sans">
-    <header class="flex">
-        <h1 class="text-2xl text-center">ENVÍANOS TUS DUDAS</h1>
-        <div class="flex-1 flex justify-end">
-            <button class="rounded-full bg-black text-white hover:bg-white hover:text-black">Ver dudas</button>
-        </div>
+    <header class="grid grid-cols-3 items-center w-full p-10">
+    <div class="flex justify-start">
+        <a href="{{ route('index') }}">
+        <button class="rounded-2xl bg-black text-white hover:bg-white hover:text-black cursor-pointer px-7 py-3 shadow-md">
+            Inicio
+        </button>
+        </a>
+    </div>
+
+    <h1 class="text-5xl text-center font-bold drop-shadow-lg">ENVÍANOS TUS DUDAS</h1>
+
+    <div class="flex justify-end">
+        <button class="rounded-2xl bg-black text-white hover:bg-white hover:text-black cursor-pointer px-7 py-3 shadow-md">
+        Ver dudas
+        </button>
+    </div>
     </header>
-    <form action="{{ route('formulario.store') }}" method="post">
-        <div>
-            <label for="email">Introduzca el correo: </label> <br>
-            <input type="text" id="email" name="email" placeholder="ejemplo@gmail.com">
-        </div>
+    <main class="flex grow flex-col items-center p-8">
+        <form action="{{ route('formulario.store') }}" method="post" class="bg-white/90 rounded-2xl shadow-2xl p-10 w-full max-w-2xl space-y-6 border border-white/30">
+            <div>
+                <label for="email">Introduzca el correo: </label> <br>
+                <input type="text" id="email" name="email" placeholder="ejemplo@gmail.com">
+            </div>
 
-        <div>
-            <label for="topic">Introduzca el asunto si lo desea: </label> <br>
-            <input type="text" id="topic" name="topic" placeholder="Esto es un formulario genial">
-        </div>
+            <div>
+                <label for="topic">Introduzca el asunto si lo desea: </label> <br>
+                <input type="text" id="topic" name="topic" placeholder="Esto es un formulario genial">
+            </div>
 
-        <div>
-            <label for="description">Escribe una descripción: </label>
-            <textarea id="description" name="description" rows="4" cols="50" placeholder="Escriba alguna sugerencia de como quieres que sea la formación"></textarea>
-        </div>
+            <div>
+                <label for="description">Escribe una descripción: </label>
+                <textarea id="description" name="description" rows="4" cols="50" placeholder="Escriba alguna sugerencia de como quieres que sea la formación"></textarea>
+            </div>
 
-        <input type="submit" value="ENVIAR">
-    </form>
+            <input type="submit" value="ENVIAR">
+        </form>
+    </main>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use App\Http\Controllers\FormController;
 
 Route::get('/', function () {
     return view('inicio');
-});
+})->name('index');
 
 Route::get('/formulario/enviar', [FormController::class, 'create'])->name('formulario.create');
 Route::post('/formulario/guardar', [FormController::class, 'store'])->name('formulario.store');
