3210519fe76cfaab50e69734d95052f61762fc46
YeroverAli <yeroveraligonzalez@alumno.ieselrincon.es>
2025-11-13 13:54:27 +0000

    Comentarios

--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,11 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\FormController;
 
+// Ruta principal ('/') que carga la vista "inicio"
 Route::get('/', function () {
     return view('inicio');
 })->name('index');
 
+// Ruta tipo "resource" que genera automáticamente todas las rutas CRUD
+// para el controlador FormController (index, create, store, show, edit, update, destroy)
 Route::resource('formulario', FormController::class); 
