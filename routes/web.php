005a34b0d0ef6c4dc92c2cbbbabdf86e081822f1
BrianDev06 <brianvegacalvo@alumno.ieselrincon.es>
2025-11-13 13:57:42 +0000

    JetStream + Mailtrap configurado

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,12 @@ Route::get('/', function () {
 })->name('index');
 
 Route::resource('formulario', FormController::class); 
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
