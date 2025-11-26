b915608144a5b322eb261f12ddd185972c20a4ef
BrianDev06 <brianvegacalvo@alumno.ieselrincon.es>
2025-11-26 10:29:05 +0000

    Instalación adminLTE

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\FormController;
+use App\Http\Controllers\AdminController;
 
 // Ruta principal ('/') que carga la vista "inicio"
 Route::get('/', function () {
@@ -12,3 +13,6 @@ Route::get('/', function () {
 // Ruta tipo "resource" que genera automáticamente todas las rutas CRUD
 // para el controlador FormController (index, create, store, show, edit, update, destroy)
 Route::resource('formulario', FormController::class); 
+
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin.index');
