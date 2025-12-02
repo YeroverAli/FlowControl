f5a460d12333e63e112532e25d65d0ff72d18a1f
BrianDev06 <brianvegacalvo@alumno.ieselrincon.es>
2025-12-02 12:21:50 +0000

    Método Store UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -14,4 +14,22 @@ class UserController extends Controller
     {
         return view('admin.users.create');
     }
+
+    public function store(Request $request)
+    {
+        // Validamos los datos recibidos
+        $datos = $request->validate([
+            'name' => 'required|string|max:50',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8|confirmed',    // confirmed verifica que exista un campo password_confirmation con el mismo valor
+        ]);
+
+        $user = new User();
+        $user->name = $datos['name'];
+        $user->email = $datos['email'];
+        $user->password = bcrypt($datos['password']);
+        $user->save();
+
+        return redirect()->route('admin.users.create')->with('success', 'Usuario creado correctamente.');
+    }
 }
